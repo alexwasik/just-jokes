ccfec24a1149abe15d7f58b5b991861dfddf67ae
Alex Wasik <arthur.wasik@gmail.com>
2021-12-10 11:41:42 -0500

    Add YoMomma Jokes Task Add dependency for Fadyazmy/yoMamma for yoMamaJokes
    Solution
    Add dependency
    Add methods to utils for yoMamaJokes
    Add Tests
    Update README.md

--- src/__tests__/utils/index.test.ts
+++ src/__tests__/utils/index.test.ts
@@ -1,4 +1,4 @@
-import { getRandomJoke, getRandomJokes } from '../../utils/index';
+import { getRandomJoke, getRandomJokes, getYoMommaJoke, getYoMommaJokes } from '../../utils/index';
   const jokesObjectMatch = {
     id: expect.any(Number),
     type: expect.any(String),
@@ -45,5 +45,59 @@ describe('get random jokes', () => {
   test('should throw error if param provided is less than 1', () => {
     expect(() => getRandomJokes(0)).toThrowError('Number of jokes must be greater than 0');
     expect(() => getRandomJokes(-1)).toThrowError('Number of jokes must be greater than 0');
+  })
+})
+
+  const yoMommaJokeObjectMatch = {
+    id: expect.any(Number),
+    type: expect.stringContaining('yo-momma'),
+    setup: expect.extend({
+      toBeUndefined() {
+        return this.isUndefined()
+      }
+    }),
+    punchline: expect.any(String),
+  }
+
+describe('get random yoMomma joke', () => {
+  test('return random joke object', () => {
+    const joke = getYoMommaJoke();
+    console.log(joke);
+    expect(joke).toBeInstanceOf(Object);
+    expect(joke).toMatchObject(yoMommaJokeObjectMatch)
+  })
+})
+
+describe('get random yoMomma jokes', () => {
+  test('return array of random yoMomma jokes', () => {
+    const JOKES_REQUESTED = 3;
+    const jokes: object[] = getYoMommaJokes(JOKES_REQUESTED);
+    expect(jokes).toBeInstanceOf(Array);
+    expect(jokes).toHaveLength(JOKES_REQUESTED);
+    jokes.forEach(joke => {
+      expect(joke).toMatchObject(yoMommaJokeObjectMatch)
+    })
+  })
+  test('return max number of 50 random yoMomma jokes', () => {
+    const JOKES_REQUESTED = 500;
+    const MAX_JOKES = 50;
+    const jokes = getYoMommaJokes(JOKES_REQUESTED);
+    expect(jokes).toHaveLength(MAX_JOKES);
+    expect(jokes).toBeInstanceOf(Array);
+    jokes.forEach(joke => {
+      expect(joke).toMatchObject(yoMommaJokeObjectMatch)
+    })
+  })
+  test('return only one joke if no param provided', () => {
+    const jokes = getYoMommaJokes();
+    expect(jokes).toHaveLength(1);
+    expect(jokes).toBeInstanceOf(Array);
+    jokes.forEach(joke => {
+      expect(joke).toMatchObject(yoMommaJokeObjectMatch)
+    })
+  })
+  test('should throw error if param provided is less than 1', () => {
+    expect(() => getYoMommaJokes(0)).toThrowError('Number of jokes must be greater than 0');
+    expect(() => getYoMommaJokes(-1)).toThrowError('Number of jokes must be greater than 0');
   })
 })
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,18 @@
 import jokes from '../jokes/jokes.json';
+const yoMomma = require('yo-mamma').default;
+
+const DICT_MAX: number = 1039;
+const MAX_JOKE_COUNT: number = 50;
+
+
+const uniqueIndex = (maxNr: number) => {
+  let array: number[] = []
+  for (let i = 0; i < maxNr; i++) {
+    let random = Math.floor(Math.random() * DICT_MAX)
+    array.push(random)
+  }
+  return array
+}
 
 const getRandomJoke = () => {
   const randomIndex: number = Math.floor(Math.random() * jokes.length);
@@ -9,7 +23,6 @@ const getRandomJokes = (numberOfJokes: number = 1) => {
   if (numberOfJokes <= 0) {
     throw new Error('Number of jokes must be greater than 0');
   }
-  const MAX_JOKE_COUNT: number = 50;
   const randomJokes: object[] = [];
   const fetchJokes = (count: number) => {
     for (let i = 0; i < count; i++) {
@@ -24,4 +37,41 @@ const getRandomJokes = (numberOfJokes: number = 1) => {
   return fetchJokes(numberOfJokes);
 }
 
-export { getRandomJoke, getRandomJokes };
+const getYoMommaJoke = () => {
+  const random_index: number = Math.floor(Math.random() * DICT_MAX);
+  const joke = yoMomma(random_index);
+  return {
+    id: random_index,
+    type: 'yo-momma',
+    setup: undefined,
+    punchline: joke
+  }
+}
+
+const getYoMommaJokes = (numberOfJokes: number = 1): object[] => {
+  if (numberOfJokes <= 0) {
+    throw new Error('Number of jokes must be greater than 0');
+  }
+
+  const randomJokes: object[] = [];
+  const fetchJokes = (count: number) => {
+    const jokes = uniqueIndex(count);
+    jokes.forEach(jokeIndex => {
+      const joke = yoMomma(jokeIndex);
+      randomJokes.push({
+        id: jokeIndex,
+        type: 'yo-momma',
+        setup: undefined,
+        punchline: joke
+      });
+    });
+    console.log('randomJokes length', randomJokes.length);
+    return randomJokes;
+  }
+  if (numberOfJokes > MAX_JOKE_COUNT) {
+    return fetchJokes(MAX_JOKE_COUNT);
+  }
+  return fetchJokes(numberOfJokes);
+}
+
+export { getRandomJoke, getRandomJokes, getYoMommaJoke, getYoMommaJokes };
