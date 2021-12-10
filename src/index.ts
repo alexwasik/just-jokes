d32a2dd0ba547203bec0ff31d7422d9d03d2acb1
Alex Wasik <arthur.wasik@gmail.com>
2021-12-10 14:07:42 -0500

    1.0.0

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,11 @@ const randomJokes = (range: number) => {
 const yoMommaJoke = () => {
   const joke = getYoMommaJoke();
   return joke;
-}
+};
 
 const yoMommaJokes = (range: number) => {
   const jokes = getYoMommaJokes(range);
   return jokes;
-}
+};
 
 export { randomJoke, randomJokes, yoMommaJoke, yoMommaJokes };
